c6c15d394ca1c9012fe4fa764d53975332af83c3
Chamal Jayasuriya <chamal@flatorb.com>
2017-04-12 19:49:56 +0530

    Initial Commit

---
+++ Login/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Login
+//
+//  Created by Chamal Jayasuriya on 4/12/17.
+//  Copyright © 2017 Chamal Jayasuriya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
